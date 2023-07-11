8f12c95cb7ad3bb5c1fb842dfe70625304717c83
Avneet Sekhon <avneetsekhon88@gmail.com>
2023-07-11 13:04:49 -0700

    Initial commit

---
+++ A-list/A-list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A-list
+//
+//  Created by Avneet Sekhon on 2023-07-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
