94b4429772de977070d4da979ec227b301ef7826
Avneet Sekhon <avneetsekhon88@gmail.com>
2023-07-13 13:07:59 -0700

    Save data with CoreData

--- A-list/A-list/Controller/ListViewController.swift
+++ A-list/A-list/Controller/ListViewController.swift
@@ -6,17 +6,21 @@
 //
 
 import UIKit
+import CoreData
 
 class ListViewController: UITableViewController {
     
     var itemArray = [DataManager]()
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Data.plist")
+    //Access to the AppDelegate.swift as an object so we can use NSPersistentContainer
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        
         //Load persistent data
-        loadDataItems()
+        //loadDataItems()
     }
     
     //Number of cells that will be created in the table view.
@@ -24,6 +28,7 @@ class ListViewController: UITableViewController {
         return itemArray.count
     }
     
+    //Cell modifications
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //Create the cell
         let cell = tableView.dequeueReusableCell(withIdentifier: "ListItemCell", for: indexPath)
@@ -39,6 +44,7 @@ class ListViewController: UITableViewController {
         return cell
     }
     
+    //Cell selection
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //Assign checkmark bool
         itemArray[indexPath.row].checked = !itemArray[indexPath.row].checked
@@ -49,15 +55,17 @@ class ListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    //New list item
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
         var newItemText = UITextField()
         let newItemAlert = UIAlertController(title: "Add new item", message: "", preferredStyle: .alert)
         let newItemAction = UIAlertAction(title: "Add item", style: .default) { newItemAction in
-            
+
             //User pressed "Add item" button, process the following:
-            let newData = DataManager()
+            let newData = DataManager(context: self.context)
             newData.title = newItemText.text!
+            newData.checked = false
             self.itemArray.append(newData)
             
             self.saveDataItems()
@@ -72,29 +80,28 @@ class ListViewController: UITableViewController {
         present(newItemAlert, animated: true, completion: nil)
     }
     
+    //Save list items to a plist
     func saveDataItems() {
-        //Encode instances of DataManager data types to a property list
-        let encoder = PropertyListEncoder()
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            try context.save()
         } catch {
-            print("Error encoding: \(error)")
+            print("Error saving context: \(error)")
         }
         
         //Reload table view
         self.tableView.reloadData()
     }
     
-    func loadDataItems() {
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do {
-                itemArray = try decoder.decode([DataManager].self, from: data)
-            } catch {
-                print("Error decoding data items: \(error)")
-            }
-        }
-    }
+    //Load items from a plist
+//    func loadDataItems() {
+//        if let data = try? Data(contentsOf: dataFilePath!) {
+//            let decoder = PropertyListDecoder()
+//            do {
+//                itemArray = try decoder.decode([DataManager].self, from: data)
+//            } catch {
+//                print("Error decoding data items: \(error)")
+//            }
+//        }
+//    }
 }
 
